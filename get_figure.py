00923205caea507747873b9eb087c324f97f5be4
Abecid <cmlee3211@gmail.com>
2026-01-20 13:31:23 +0000

    Fix hand flipped save in separaet directory

--- get_figure.py
+++ get_figure.py
@@ -6,8 +6,10 @@ import cv2
 def main():
     pass
 
-def flip(images_path="demo_out/01_19_13_44"):
+def flip(images_path="demo_out/01_20_13_01/295"):
     # flip image horizontally
+    out_path = f"{images_path}/hand_flipped"
+    os.makedirs(out_path, exist_ok=True)
     hand_paths = [p for p in os.listdir(images_path) if "_hand.png" in p]
     for hand_path in tqdm(hand_paths):
         in_path = os.path.join(images_path, hand_path)
@@ -16,8 +18,10 @@ def flip(images_path="demo_out/01_19_13_44"):
             print(f"Skipping (failed to read): {in_path}")
             continue
         flipped = cv2.flip(img, 1)  # 1 = horizontal flip
-        out_path = in_path.replace("hand", "hand_flipped")
-        ok = cv2.imwrite(out_path, flipped)
+        # out_path = in_path.replace("hand", "hand_flipped")
+        img_out_path = f"{out_path}/{hand_path}"
+        
+        ok = cv2.imwrite(img_out_path, flipped)
 
 
 if __name__ == "__main__":
